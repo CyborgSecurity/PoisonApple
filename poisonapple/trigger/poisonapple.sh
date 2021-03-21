a67e15af756f0fceafc6c27498ae3a0b995ace32
vesche <vesche@protonmail.com>
2021-03-21 12:08:05 -0500

    improve trigger shell script

--- poisonapple/trigger/poisonapple.sh
+++ poisonapple/trigger/poisonapple.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Triggered @ $(date)" >> /Users/$(logname)/Desktop/PoisonApple-$1
+echo "Triggered @ $(date) " >> /Users/$(stat -f "%Su" /dev/console)/Desktop/PoisonApple-$1
